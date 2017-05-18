cf85f64c18afc42cdbcd435462ca9c1269ebedde
PiotrZdunek <piotrasz@op.pl>
2017-05-19 00:10:58 +0200

    poprawiony widko kina

--- src/main/webapp/app/main/cinema/order.controller.js
+++ src/main/webapp/app/main/cinema/order.controller.js
@@ -5,14 +5,14 @@
         .module('myApp')
         .controller('OrderController', OrderController);
 
-    OrderController.$inject = ['$uibModalInstance'];
+    OrderController.$inject = ['$uibModalInstance', '$scope'];
 
-    function OrderController ($uibModalInstance) {
+    function OrderController ($uibModalInstance, $scope) {
         var vm = this;
 
         
-        var reserved = ['A2', 'A3', 'C5', 'C6', 'C7', 'C8', 'J1', 'J2', 'J3', 'J4'];
-        var selected = [];
+        // var reserved = ['A2', 'A3', 'C5', 'C6', 'C7', 'C8', 'J1', 'J2', 'J3', 'J4'];
+        // var selected = [];
 
         vm.clear = clear;
         
@@ -23,6 +23,51 @@
 
         vm.submitSeats = function(){
             console.log("wyslano")
+        }
+
+
+         // Init layout
+        $scope.rows = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'];
+        $scope.cols = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+
+        // Set reserved and selected
+        var reserved = ['A2', 'A3', 'C5', 'C6', 'C7', 'C8', 'J1', 'J2', 'J3', 'J4','A1', 'F7'];
+        var selected = [];
+
+        // seat onClick
+        $scope.seatClicked = function(seatPos) {
+            console.log("Selected Seat: " + seatPos);
+            var index = selected.indexOf(seatPos);
+            if(index != -1) {
+                // seat already selected, remove
+                selected.splice(index, 1)
+            } else {
+                // new seat, push
+                selected.push(seatPos);
+            }
+        }
+
+        // get seat status
+        $scope.getStatus = function(seatPos) {
+            if(reserved.indexOf(seatPos) > -1) {
+                return 'reserved';
+            } else if(selected.indexOf(seatPos) > -1) {
+                return 'selected';
+            }
+        }
+
+        // clear selected
+        $scope.clearSelected = function() {
+            selected = [];
+        }
+
+        // show selected
+        $scope.showSelected = function() {
+            if(selected.length > 0) {
+                alert("Selected Seats: \n" + selected);
+            } else {
+                alert("No seats selected!");
+            }
         }
 
     }
